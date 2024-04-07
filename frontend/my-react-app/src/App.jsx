eb950ad9af2f0a95c8e1f36847c509a831a5dd16
efei36 <78135049+efei36@users.noreply.github.com>
2024-04-07 13:27:10 -0500

    Add

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,8 +1,34 @@
-import { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import './App.css'
+import { initializeApp } from "firebase/app";
+import { ref, onValue, getDatabase } from "firebase/database";
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBth9GJ1ig1dLcgxubtwzG3vuoocJJmjDo",
+  authDomain: "elon-tweet-analysis.firebaseapp.com",
+  databaseURL: "https://elon-tweet-analysis-default-rtdb.firebaseio.com",
+  projectId: "elon-tweet-analysis",
+  storageBucket: "elon-tweet-analysis.appspot.com",
+  messagingSenderId: "362434812086",
+  appId: "1:362434812086:web:582d32a5607d69e0f76e17",
+  measurementId: "G-ND78YWVZGS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+  
+  useEffect(() => {
+    const database = getDatabase(app);
+    const dbref = ref(database, '/');
+
+    onValue(dbref, (snapshot) => {
+      setData(snapshot.val());
+    });
+  }, []);
 
   return (
     <>
@@ -36,8 +62,11 @@ function App() {
         <h2>Last 100 Tweets</h2>
         <div className="scrollable-container">
           <div className="scrollable-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at risus neque. Integer euismod orci a libero rhoncus facilisis. Duis convallis convallis tellus, eu semper ipsum tincidunt eget. In turpis augue, vestibulum ac augue et, consequat aliquet risus. Quisque ac urna et velit ornare scelerisque. Suspendisse potenti. Proin id libero mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum commodo nulla eu augue fringilla tristique. Ut volutpat malesuada libero, non gravida arcu euismod eu. Quisque eget urna vel erat ullamcorper varius. Etiam malesuada velit bibendum mi bibendum, sit amet mollis dolor vestibulum. Proin vitae elit non felis placerat ultricies. Cras tempor quam a mollis varius. Cras semper risus vel purus convallis, in posuere ipsum tempor.</p>
-            <p>Integer pharetra enim id lectus euismod posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque auctor justo quis orci facilisis aliquet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam et diam ac quam tincidunt interdum. In hac habitasse platea dictumst. Sed ut enim ac quam posuere lacinia. Ut at lorem elit. Donec pretium non arcu ut bibendum. Donec convallis ligula et venenatis dapibus. Sed nec semper metus. Sed in nisi malesuada, tempus arcu non, consequat lorem. Ut lacinia pretium libero, nec pretium purus venenatis nec. Nullam accumsan, nunc id scelerisque lobortis, felis risus varius orci, eget ultrices dolor nisi quis dui. Donec nec tellus magna. Suspendisse potenti.</p>
+          {data ? (
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          ) : (
+            <p>Loading...</p>
+          )}
           </div>
         </div>
       </div>
