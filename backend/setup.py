137db0a12d40689ac74aa07aa6528b647aa09973
Golf0ned <benjaminye.email@gmail.com>
2024-04-07 11:30:20 -0500

    hey look another setup update!

--- backend/setup.py
+++ backend/setup.py
@@ -2,6 +2,7 @@ import nltk
 
 print('Downloading nltk dependencies...')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
 nltk.download('punkt')
 nltk.download('vader_lexicon')
 print('Done!')
