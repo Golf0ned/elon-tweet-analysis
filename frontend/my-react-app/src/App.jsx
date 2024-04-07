79cb27cf9fdc26904eea8798056926273af5a501
efei36 <78135049+efei36@users.noreply.github.com>
2024-04-07 11:36:40 -0500

    Updated webapp

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,22 +6,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Elon Tweet Analysis</h1>
+      <h2>Making stock predictions based off the things Elon says</h2>
+      <div class="column-container">
+        <div class="column">
+          <div class="displayer">
+            <h2>Most Recent Tweet</h2>
+          </div>
+        </div>
+        <div class="column">
+          <div class="displayer">
+            <h2>Recommendations</h2>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="displayer">
+        <h2>Last 100 Tweets</h2>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
